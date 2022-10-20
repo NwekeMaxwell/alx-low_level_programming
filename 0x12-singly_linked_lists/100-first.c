809e0e656db81a14f92e362afb30adf2e94b3f2d
Nweke Maxwell <nwekemaxwell37@gmail.com>
2022-10-20 14:21:25 +0100

    a function that prints ^ before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+es (10 sloc)  240 Bytes
+
+#include <stdio.h>
+
+/**
+ * bmain - function executed before main
+ * Return: no return.
+ */
+
+void __attribute__((constructor)) bmain()
+{
+    printf("You're beat! and yet, you must allow");
+    printf(",\nI bore my house upon my back!\n");
+}
